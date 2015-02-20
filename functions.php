ba74c5e1587f8a6558dfe9131c5059f80259f4fe
David <david@davidburgess.co>
2015-02-19 18:07:03 -0700

    added suckit function to functions.php

--- functions.php
+++ functions.php
@@ -329,3 +329,7 @@ require get_template_directory() . '/inc/template-tags.php';
  * @since Twenty Fifteen 1.0
  */
 require get_template_directory() . '/inc/customizer.php';
+
+function suck_it() {
+	echo 'Suck It!';
+}
